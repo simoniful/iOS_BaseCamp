dffe5d0580a6b760edc072c2f3560875c7ad69b6
sanghunlee <simoniful0@gmail.com>
2022-12-28 23:14:50 +0900

    Add: Tourist Request Query 작성

--- Basecamp/Domain/Model/Request/TouristInfoQuery.swift
+++ Basecamp/Domain/Model/Request/TouristInfoQuery.swift
@@ -14,4 +14,82 @@ enum TouristInfoQueryType {
   case keyword(numOfRows: Int, pageNo: Int, contentTypeId: Int, keyword: String)
   case festival(numOfRows: Int, pageNo: Int, areaCode: Int, sigunguCode: Int, eventStartDate: String, eventEndDate: String)
   case stay(numOfRows: Int, pageNo: Int, areaCode: Int, sigunguCode: Int)
+  case commonInfo(contentId: Int, contentTypeId: Int)
+  case introInfo(contentId: Int, contentTypeId: Int)
+  case detailInfo(contentId: Int, contentTypeId: Int)
+  case imageList(contentId: Int)
+  case regionCode(numOfRows: Int, pageNo: Int, areaCode: Int)
+  
+  var query: TouristInfoQuery {
+    switch self {
+    case .categoryCode(let numOfRows, let pageNo, let contentTypeId, let cat1, let cat2, let cat3):
+      <#code#>
+    case .region(let numOfRows, let pageNo, let contentTypeId, let areaCode, let sigunguCode, let cat1, let cat2, let cat3, let modifiedtime):
+      <#code#>
+    case .location(let numOfRows, let pageNo, let coordinate, let radius):
+      <#code#>
+    case .keyword(let numOfRows, let pageNo, let contentTypeId, let keyword):
+      <#code#>
+    case .festival(let numOfRows, let pageNo, let areaCode, let sigunguCode, let eventStartDate, let eventEndDate):
+      <#code#>
+    case .stay(let numOfRows, let pageNo, let areaCode, let sigunguCode):
+      <#code#>
+    case .commonInfo(let contentId, let contentTypeId):
+      <#code#>
+    case .introInfo(let contentId, let contentTypeId):
+      <#code#>
+    case .detailInfo(let contentId, let contentTypeId):
+      <#code#>
+    case .imageList(let contentId):
+      <#code#>
+    case .regionCode(let numOfRows, let pageNo, let areaCode):
+      <#code#>
+    }
+  }
+}
+
+protocol TouristInfoQuery {}
+
+struct CategoryCodeQuery: TouristInfoQuery {
+  
+}
+
+struct TouristInfoRegionQuery: TouristInfoQuery {
+  
+}
+
+struct TouristInfoLocationQuery: TouristInfoQuery {
+  
+}
+
+struct TouristInfoKeywordQuery: TouristInfoQuery {
+  
+}
+
+struct TouristInfoFestivalQuery: TouristInfoQuery {
+  
+}
+
+struct TouristInfoStayQuery: TouristInfoQuery {
+  
+}
+
+struct TouristInfoCommonQuery: TouristInfoQuery {
+  
+}
+
+struct TouristInfoDetailQuery: TouristInfoQuery {
+  
+}
+
+struct TouristInfoIntroQuery: TouristInfoQuery {
+  
+}
+
+struct TouristInfoimageListQuery: TouristInfoQuery {
+  
+}
+
+struct TouristInfoRegionCodeQuery: TouristInfoQuery {
+  
 }
